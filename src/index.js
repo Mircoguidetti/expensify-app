5d79b1225814e21d083e2b87fd2dfe53f052e0eb
Mircoguidetti <mirco@guidetti.co>
2019-05-19 19:12:18 +0100

    Connected react with redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/configureStore';
 import IndexRouter from './routers/IndexRouter';
-
 import './styles/main.scss';
+
+const store = configureStore()
+console.log(store.getState())
 
 ReactDOM.render(<IndexRouter />, document.getElementById('app'));
